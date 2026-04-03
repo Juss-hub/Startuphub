20f28978f7b3bfde411ea4685ec27af67aa92127
Juss-hub <moininajuniors@gmail.com>
2026-04-02 14:45:29 -1200

    added about page

---
+++ app/about/page.tsx
@@ -0,0 +1,3 @@
+export default function AboutPage() {
+  return <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta quisquam saepe harum, atque reiciendis eveniet quasi officia expedita. Temporibus, quas! Numquam ipsum minus dolorem eos, iure doloribus. Sunt, ex provident?</div>;
+}
